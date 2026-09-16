c6e05e77ae503d1c6e29a31723313134d458b058
RaquelAires <raquel.airespsilva@gmail.com>
2026-09-16 19:19:57 -0300

    verificando novamente

--- js/api.js
+++ js/api.js
@@ -1,16 +1,33 @@
 export async function carregarTarefas() {
 
-    const resposta = await fetch("../dados.json");
+    const resposta =
+        await fetch("./dados.json");
+
 
     if (!resposta.ok) {
-        throw new Error(`HTTP ${resposta.status}`);
+
+        throw new Error(
+            `HTTP ${resposta.status}`
+        );
+
     }
 
-    const dados = await resposta.json();
 
-    if (!dados || !Array.isArray(dados.tarefas)) {
-        throw new SyntaxError("O arquivo JSON não possui uma lista de tarefas válida.");
+    const dados =
+        await resposta.json();
+
+
+    if (
+        !dados ||
+        !Array.isArray(dados.tarefas)
+    ) {
+
+        throw new SyntaxError(
+            "O arquivo JSON não possui uma lista de tarefas válida."
+        );
+
     }
+
 
     return dados.tarefas;
 }
